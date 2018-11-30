241ba369c2f9e17150d874c201e7a62f6c2e8f4b
JamesPZilverberg <jpzilver@gmail.com>
2018-11-29 21:17:48 -0600

    simplebotframework

--- catan_utils/simple_bot.py
+++ catan_utils/simple_bot.py
@@ -4,11 +4,64 @@ from abc import ABCMeta, abstractmethod
 class Bot(metaclass=ABCMeta):
     def __init__(self):
         self.resources = {'wood': 0, 'wheat':0, 'brick':0, 'ore':0, 'sheep':0}
+        self.dev_cards = {'knight':0, 'monopoly':0, 'year of plenty':0, 'road builder':0, 'victory point':0}
+        self.longest_road = False
+        self.largest_army = False
 
+    """Phase 0 Functions"""
     @abstractmethod
-    def evaluate_trade_request(self):
+    def receive_rules(self):
         pass
 
+    """Phase 1 Functions"""
+    @abstractmethod
+    def place_initial_settlement(self, game_state, available_moves):
+        """
+        Needs to return a settlement location and a connecting road.  Move needs to be valid.
+        Will run twice
+        :return: settlement_location, road_location
+        """
+        pass
+
+    """Phase 2 Functions"""
+    @abstractmethod
+    def make_a_move(self, game_state, available_moves):
+        """
+        Returning None will end your turn.  Trade, build, or play dev card.
+        Available moves will be structured as:
+            availabe_moves = {buy:{road:[available_road_locations],
+                                   settlement:[available_settlement_locations],
+                                   city:[available_city_locations],
+                                   dev_card:True},
+                              play:self.dev_cards,
+                              trade:[player_ids]}
+
+
+        :return: move
+        """
+        pass
+
+    @abstractmethod
+    def propose_trade_request(self, game_state, player_id, your_resources):
+        """
+        Propose two trades to each player. Unless excepted on first one.
+        :param player_id:
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def evaluate_trade_request(self, game_state, player_id, your_resources):
+        pass
+
+    @abstractmethod
+    def event_notification(self, event):
+        """
+        Each player is notified of all public events.  This function returns nothing.
+        :param event:
+        :return: None
+        """
+        pass
 
 class SimpleBot(Bot):
     """Basic Player to make moves essentially randomly.
@@ -33,5 +86,16 @@ class SimpleBot(Bot):
         super().__init__()
         pass
 
-    # def evaluate_trade_request(self):
-    #     pass
+    """Phase 1 Functions"""
+    def place_initial_settlement(self):
+        pass
+
+    """Phase 2 Function"""
+    def make_a_move(self):
+        pass
+
+    def evaluate_trade_request(self):
+        pass
+
+    def
+
